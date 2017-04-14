70efe09ff42f4f69cc72c017e24a94ae6ee37dcb
178inaba <178inaba@users.noreply.github.com>
2017-04-15 02:23:17 +0900

    Add GetBlocks

--- accounts.go
+++ accounts.go
@@ -66,6 +66,16 @@ func (c *Client) GetAccountFollowing(id int64) ([]*Account, error) {
 	return accounts, nil
 }
 
+// GetBlocks return block list.
+func (c *Client) GetBlocks() ([]*Account, error) {
+	var accounts []*Account
+	err := c.doAPI(http.MethodGet, "/api/v1/blocks", nil, &accounts)
+	if err != nil {
+		return nil, err
+	}
+	return accounts, nil
+}
+
 // Relationship hold information for relation-ship to the account.
 type Relationship struct {
 	ID         int64 `json:"id"`

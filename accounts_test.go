f5194b9ebb24366078b46301b53c00d0a73ef20e
178inaba <178inaba@users.noreply.github.com>
2017-04-17 00:04:25 +0900

    Add FollowRequestAuthorize and FollowRequestReject

--- accounts_test.go
+++ accounts_test.go
@@ -155,3 +155,51 @@ func TestGetFollowRequests(t *testing.T) {
 		t.Fatalf("want %q but %q", "bar", fReqs[0].Username)
 	}
 }
+
+func TestFollowRequestAuthorize(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/api/v1/follow_requests/1234567/authorize" {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		}
+	}))
+	defer ts.Close()
+
+	client := NewClient(&Config{
+		Server:       ts.URL,
+		ClientID:     "foo",
+		ClientSecret: "bar",
+		AccessToken:  "zoo",
+	})
+	err := client.FollowRequestAuthorize(123)
+	if err == nil {
+		t.Fatalf("should be fail: %v", err)
+	}
+	err = client.FollowRequestAuthorize(1234567)
+	if err != nil {
+		t.Fatalf("should not be fail: %v", err)
+	}
+}
+
+func TestFollowRequestReject(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/api/v1/follow_requests/1234567/reject" {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		}
+	}))
+	defer ts.Close()
+
+	client := NewClient(&Config{
+		Server:       ts.URL,
+		ClientID:     "foo",
+		ClientSecret: "bar",
+		AccessToken:  "zoo",
+	})
+	err := client.FollowRequestReject(123)
+	if err == nil {
+		t.Fatalf("should be fail: %v", err)
+	}
+	err = client.FollowRequestReject(1234567)
+	if err != nil {
+		t.Fatalf("should not be fail: %v", err)
+	}
+}

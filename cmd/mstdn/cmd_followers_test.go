db7fcb38bbed5a50877c0f1396115c4c7ac832de
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-04-16 23:52:32 +0900

    add test for followers command

---
+++ cmd/mstdn/cmd_followers_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/urfave/cli"
+)
+
+func TestCmdFollowers(t *testing.T) {
+	out := testWithServer(
+		func(w http.ResponseWriter, r *http.Request) {
+			switch r.URL.Path {
+			case "/api/v1/accounts/verify_credentials":
+				fmt.Fprintln(w, `{"id": 123}`)
+				return
+			case "/api/v1/accounts/123/followers":
+				fmt.Fprintln(w, `[{"id": 234, "username": "zzz"}]`)
+				return
+			}
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		},
+		func(app *cli.App) {
+			app.Run([]string{"mstdn", "followers"})
+		},
+	)
+	if !strings.Contains(out, "zzz") {
+		t.Fatalf("%q should be contained in output of instance command: %v", "zzz", out)
+	}
+}

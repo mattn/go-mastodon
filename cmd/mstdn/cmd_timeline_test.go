f2ab559a027b1aa74fd8037976892cee0db3de38
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-04-16 23:44:07 +0900

    add test for timeline command

---
+++ cmd/mstdn/cmd_timeline_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/urfave/cli"
+)
+
+func TestCmdTimeline(t *testing.T) {
+	out := testWithServer(
+		func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path != "/api/v1/timelines/home" {
+				http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+				return
+			}
+			fmt.Fprintln(w, `[{"content": "zzz"}]`)
+			return
+		},
+		func(app *cli.App) {
+			app.Run([]string{"mstdn", "timeline"})
+		},
+	)
+	if !strings.Contains(out, "zzz") {
+		t.Fatalf("%q should be contained in output of instance command: %v", "zzz", out)
+	}
+}

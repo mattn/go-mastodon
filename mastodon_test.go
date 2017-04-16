9f84e175f15c714d59a109e6332ae3d92580b793
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-04-16 20:05:42 +0900

    separate test file

--- mastodon_test.go
+++ mastodon_test.go
@@ -1,13 +1,11 @@
 package mastodon
 
 import (
-	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 )
 
 func TestAuthenticate(t *testing.T) {
@@ -224,57 +222,5 @@ func TestRegisterApp(t *testing.T) {
 	}
 	if app.ClientSecret != "bar" {
 		t.Fatalf("want %q but %q", "bar", app.ClientSecret)
-	}
-}
-
-func TestStreamingPublic(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/api/v1/streaming/public" {
-			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
-		}
-		f, _ := w.(http.Flusher)
-		fmt.Fprintln(w, `
-event: update
-data: {"Content": "foo"}
-		`)
-		f.Flush()
-
-		fmt.Fprintln(w, `
-event: update
-data: {"Content": "bar"}
-		`)
-		f.Flush()
-		return
-	}))
-	defer ts.Close()
-
-	client := NewClient(&Config{
-		Server:       ts.URL,
-		ClientID:     "foo",
-		ClientSecret: "bar",
-		AccessToken:  "zoo",
-	})
-	ctx, cancel := context.WithCancel(context.Background())
-	q, err := client.StreamingPublic(ctx)
-	if err != nil {
-		t.Fatalf("should not be fail: %v", err)
-	}
-	time.AfterFunc(3*time.Second, func() {
-		cancel()
-		close(q)
-	})
-	events := []Event{}
-	for e := range q {
-		events = append(events, e)
-	}
-	if len(events) != 2 {
-		t.Fatalf("result should be two: %d", len(events))
-	}
-	if events[0].(*UpdateEvent).Status.Content != "foo" {
-		t.Fatalf("want %q but %q", "foo", events[0].(*UpdateEvent).Status.Content)
-	}
-	if events[1].(*UpdateEvent).Status.Content != "bar" {
-		t.Fatalf("want %q but %q", "bar", events[1].(*UpdateEvent).Status.Content)
 	}
 }

1495b1fb025cb2f9160b750ee43998fc6754abb9
178inaba <178inaba@users.noreply.github.com>
2017-04-16 17:11:47 +0900

    Add TestGetStatus

--- status_test.go
+++ status_test.go
@@ -35,6 +35,36 @@ func TestGetFavourites(t *testing.T) {
 	}
 }
 
+func TestGetStatus(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/api/v1/statuses/1234567" {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		fmt.Fprintln(w, `{"Content": "zzz"}`)
+		return
+	}))
+	defer ts.Close()
+
+	client := NewClient(&Config{
+		Server:       ts.URL,
+		ClientID:     "foo",
+		ClientSecret: "bar",
+		AccessToken:  "zoo",
+	})
+	_, err := client.GetStatus(123)
+	if err == nil {
+		t.Fatalf("should be fail: %v", err)
+	}
+	status, err := client.GetStatus(1234567)
+	if err != nil {
+		t.Fatalf("should not be fail: %v", err)
+	}
+	if status.Content != "zzz" {
+		t.Fatalf("want %q but %q", "zzz", status.Content)
+	}
+}
+
 func TestGetRebloggedBy(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.URL.Path != "/api/v1/statuses/1234567/reblogged_by" {

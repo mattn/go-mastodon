dfbfecd32641e0c38814c09223e5494dc3ea4bea
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-04-16 22:27:54 +0900

    add followers command

---
+++ cmd/mstdn/cmd_followers.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mattn/go-mastodon"
+	"github.com/urfave/cli"
+)
+
+func cmdFollowers(c *cli.Context) error {
+	client := c.App.Metadata["client"].(*mastodon.Client)
+	account, err := client.GetAccountCurrentUser()
+	if err != nil {
+		return err
+	}
+	followers, err := client.GetAccountFollowers(account.ID)
+	if err != nil {
+		return err
+	}
+	for _, follower := range followers {
+		fmt.Printf("%v,%v\n", follower.ID, follower.Username)
+	}
+	return nil
+}

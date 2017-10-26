2866ebbdc5489783793dc6f09082ff3ad77020a2
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2017-10-26 15:02:33 +0900

    fix test

--- status_test.go
+++ status_test.go
@@ -71,10 +71,10 @@ func TestGetStatus(t *testing.T) {
 		t.Fatalf("want %q but %q", "💩", status.Emojis[0])
 	}
 	if status.Emojis[0].URL != "http://example.com" {
-		t.Fatalf("want %q but %q", "💩", "https://example.com")
+		t.Fatalf("want %q but %q", "https://example.com", static.Emojis[0].URL)
 	}
 	if status.Emojis[0].StaticURL != "http://example.com/static" {
-		t.Fatalf("want %q but %q", "💩", "https://example.com/static")
+		t.Fatalf("want %q but %q", "https://example.com/static", static.Emojis[0].URL)
 	}
 }
 

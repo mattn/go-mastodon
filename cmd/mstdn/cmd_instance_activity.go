0022a536491584edc2132b4e39b5e6e547b6d3e6
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-01-29 13:15:06 +0900

    add instance_activity/instance_peers command

---
+++ cmd/mstdn/cmd_instance_activity.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/mattn/go-mastodon"
+	"github.com/urfave/cli"
+)
+
+func cmdInstanceActivity(c *cli.Context) error {
+	client := c.App.Metadata["client"].(*mastodon.Client)
+	activities, err := client.GetInstanceActivity(context.Background())
+	if err != nil {
+		return err
+	}
+	for _, activity := range activities {
+		fmt.Fprintf(c.App.Writer, "Logins        : %v\n", activity.Logins)
+		fmt.Fprintf(c.App.Writer, "Registrations : %v\n", activity.Registrations)
+		fmt.Fprintf(c.App.Writer, "Statuses      : %v\n", activity.Statuses)
+		fmt.Fprintf(c.App.Writer, "EMail         : %v\n", activity.Week)
+	}
+	return nil
+}

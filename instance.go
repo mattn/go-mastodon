be5e83ec1599a4cc881f3edac45e9d7b9b9b6b8b
Yamagishi Kazutoshi <ykzts@desire.sh>
2018-01-29 13:09:30 +0900

    Add new attributes for instance

--- instance.go
+++ instance.go
@@ -7,10 +7,21 @@ import (
 
 // Instance hold information for mastodon instance.
 type Instance struct {
-	URI         string `json:"uri"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	EMail       string `json:"email"`
+	URI         string            `json:"uri"`
+	Title       string            `json:"title"`
+	Description string            `json:"description"`
+	EMail       string            `json:"email"`
+	Version     string            `json:"version,omitempty"`
+	URLs        map[string]string `json:"urls,omitempty"`
+	Stats       InstanceStats     `json:"stats,omitempty"`
+	Thumbnail   string            `json:"thumbnail,omitempty"`
+}
+
+// InstanceStats hold information for mastodon instance stats.
+type InstanceStats struct {
+	UserCount   int64 `json:"user_count"`
+	StatusCount int64 `json:"status_count"`
+	DomainCount int64 `json:"domain_count"`
 }
 
 // GetInstance return Instance.

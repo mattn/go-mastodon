mattn/go-mastodon

commit b8bb5ae68c410198be4934bc93c110c666746c46

Add MinID to Pagination

mastodon.go
@@ -268,6 +268,7 @@ type Results struct {
 type Pagination struct {
 	MaxID   ID
 	SinceID ID
+	MinID   ID
 	Limit   int64
 }
 
@@ -291,6 +292,12 @@ func newPagination(rawlink string) (*Pagination, error) {
 				return nil, err
 			}
 			p.SinceID = sinceID
+
+			minID, err := getPaginationID(link.URL, "min_id")
+			if err != nil {
+				return nil, err
+			}
+			p.MinID = minID
 		}
 	}
 
@@ -323,8 +330,12 @@ func (p *Pagination) toValues() url.Values {
 func (p *Pagination) setValues(params url.Values) url.Values {
 	if p.MaxID != "" {
 		params.Set("max_id", string(p.MaxID))
-	} else if p.SinceID != "" {
+	}
+	if p.SinceID != "" {
 		params.Set("since_id", string(p.SinceID))
+	}
+	if p.MinID != "" {
+		params.Set("min_id", string(p.MinID))
 	}
 	if p.Limit > 0 {
 		params.Set("limit", fmt.Sprint(p.Limit))

mastodon_test.go
@@ -339,7 +339,7 @@ func TestPaginationSetValues(t *testing.T) {
 	if after.Get("max_id") != "123" {
 		t.Fatalf("want %q but %q", "123", after.Get("max_id"))
 	}
-	if after.Get("since_id") != "" {
+	if after.Get("since_id") != "789" {
 		t.Fatalf("result should be empty string: %q", after.Get("since_id"))
 	}
 	if after.Get("limit") != "10" {

commit acbaf58d8ae63001b067b322cbd89ff4e0bc0828

Fixed pagination regression
Pagination requests have been broken since b8bb5ae68.
Only one of the parameters `max_id`, `since_id` or `min_id` must be supplied.

mastodon.go
@@ -372,11 +372,9 @@ func (p *Pagination) toValues() url.Values {
 func (p *Pagination) setValues(params url.Values) url.Values {
 	if p.MaxID != "" {
 		params.Set("max_id", string(p.MaxID))
-	}
-	if p.SinceID != "" {
+	} else if p.SinceID != "" {
 		params.Set("since_id", string(p.SinceID))
-	}
-	if p.MinID != "" {
+	} else if p.MinID != "" {
 		params.Set("min_id", string(p.MinID))
 	}
 	if p.Limit > 0 {

mastodon_test.go
@@ -332,10 +332,8 @@ func TestGetPaginationID(t *testing.T) {
 
 func TestPaginationSetValues(t *testing.T) {
 	p := &Pagination{
-		MaxID:   "123",
-		SinceID: "456",
-		MinID:   "789",
-		Limit:   10,
+		MaxID: "123",
+		Limit: 10,
 	}
 	before := url.Values{"key": {"value"}}
 	after := p.setValues(before)
@@ -345,29 +343,25 @@ func TestPaginationSetValues(t *testing.T) {
 	if after.Get("max_id") != "123" {
 		t.Fatalf("want %q but %q", "123", after.Get("max_id"))
 	}
-	if after.Get("since_id") != "456" {
-		t.Fatalf("want %q but %q", "456", after.Get("since_id"))
-	}
-	if after.Get("min_id") != "789" {
-		t.Fatalf("want %q but %q", "789", after.Get("min_id"))
-	}
 	if after.Get("limit") != "10" {
 		t.Fatalf("want %q but %q", "10", after.Get("limit"))
 	}
 
 	p = &Pagination{
-		MaxID:   "",
+		MinID: "456",
+	}
+	before = url.Values{}
+	after = p.setValues(before)
+	if after.Get("min_id") != "456" {
+		t.Fatalf("want %q but %q", "456", after.Get("min_id"))
+	}
+
+	p = &Pagination{
 		SinceID: "789",
 	}
 	before = url.Values{}
 	after = p.setValues(before)
-	if after.Get("max_id") != "" {
-		t.Fatalf("result should be empty string: %q", after.Get("max_id"))
-	}
 	if after.Get("since_id") != "789" {
 		t.Fatalf("want %q but %q", "789", after.Get("since_id"))
-	}
-	if after.Get("min_id") != "" {
-		t.Fatalf("result should be empty string: %q", after.Get("min_id"))
 	}
 }